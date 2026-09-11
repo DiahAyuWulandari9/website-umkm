b218f47fb3c3f0daecb7685c038ec0667936e531
diahayuwulandari296@gmail.com <diahayuwulandari296@gmail.com>
2026-09-12 06:26:45 +0700

    feat(p01): buat landing page awal website umkm

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
-const promoButton = document.querySelection("#promoButton");
+// Tombol promo bagian atas
+const promoButton = document.getElementById("promoButton");
 
-promoButton.addEventListener("click", () => {
-    promoButton.textContent = "Promo: Beli 2 gratis tester!";
-    console.log("Promo Roti DAW berhasil ditampilkan");
+
+// Ketika tombol promo atas diklik
+promoButton.addEventListener("click", function () {
+    alert("Promo hari ini: Smart Snack Mix hanya Rp20.000!");
 });
